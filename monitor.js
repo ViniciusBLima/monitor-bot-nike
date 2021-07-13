37194c2e7fefa12accd6ae7d7e7e987ae78fb588
Vinicius <viniciusdelima54@gmail.com>
2021-07-13 19:32:01 -0300

    Marcação no discord configurada

--- monitor.js
+++ monitor.js
@@ -2,6 +2,9 @@ var start = new Date()
 var hrstart = process.hrtime()
 var simulateTime = 5
 var a = false;
+const ipLocal = require('ip');
+
+var ip = ipLocal.address()
 
 const api = require('./api.js');
 const discord = require('discord.js');
@@ -12,64 +15,69 @@ const discordInfo = require('./discord-info.json');
 //--------------CONECTA NO DISCORD-----------------------------
 //-------------------------------------------------------------
 //-------------------------------------------------------------
+
 const client = new discord.Client();
 const webhookClient = new discord.WebhookClient(discordInfo.hookId, discordInfo.hookToken);
-webhookClient.send("ninguém mais suporta o victor e o pedro")
 
-function callBack(error,response) {
-  if (!error && response.statusCode == 200) {
+webhookClient.send("BOT CONECTADO NO USUARIO: <@243176987584102400>, IP: "+ip);
+
+//function que monitora o tenis
+
+
+function callBack(response) {
   var retorno = JSON.parse(response)[0];
+  if (retorno.status) {
+    if (retorno.status == "AVAILABLE") {
+      var quantidadeSkus = retorno.skus.length
 
-  if (retorno.status == "AVAILABLE") {
-    var quantidadeSkus = retorno.skus.length
+      for (let i = 0; i < quantidadeSkus; i++) {
+        let sku = retorno.skus[i];
 
-    for (let i = 0; i < quantidadeSkus; i++) {
-      let sku = retorno.skus[i];
+        if (sku.status == "AVAILABLE") {
+          let tenis = {
+            nome: sku.name,
+            tamanho: sku.specs.size,
+            preco: sku.price,
+            imagem: sku.images.default.substr(2)
+          }
 
-      if (sku.status == "AVAILABLE") {        
-        let tenis = {
-          nome: sku.name,
-          tamanho: sku.specs.size,
-          preco: sku.price,
-          imagem: sku.images.default.substr(2)
-        }
-
-        webhookClient.send({
-          embeds: [{
-            color: 'FF0000',
-            thumbnail: {
-              'url': "https://" + tenis.imagem
-            },
-            title: ':flame: ITEM ' + tenis.nome + ' DISPONIVEL :flame:',
-            fields: [
-              {
-                name: 'Item:',
-                value: tenis.nome,
+          webhookClient.send({
+            embeds: [{
+              color: 'FF0000',
+              thumbnail: {
+                'url': "https://" + tenis.imagem
               },
-              {
-                name: 'Tamanho:',
-                value: tenis.tamanho
-              },
-              {
-                name: 'Preço:',
-                value: tenis.preco
-              }
-            ]
+              title: ':flame: ITEM ' + tenis.nome + ' DISPONIVEL :flame:',
+              fields: [
+                {
+                  name: 'Item:',
+                  value: tenis.nome,
+                },
+                {
+                  name: 'Tamanho:',
+                  value: tenis.tamanho
+                },
+                {
+                  name: 'Preço:',
+                  value: tenis.preco
+                }
+              ]
 
-          }]
-        });
+            }]
+          });
+        };
       };
     };
-  };
-}
-else{
-  console.log(response);
-}
+  }
+  else {
+    console.log(response);
+  }
+  //
 };
 
 
 setInterval(function () {
   api.sendRequest(callBack);
-}, 1000);
+}, 10000);
 
 
